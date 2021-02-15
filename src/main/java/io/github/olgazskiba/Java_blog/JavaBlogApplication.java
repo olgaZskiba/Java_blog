cb709aa0bea68bfb061eb70b3205dfdb2e2740f5
”olgaZskiba” <olgaz.sweetheart@gmail.com>
2021-02-15 16:30:10 +0200

    adds original pom

---
+++ src/main/java/io/github/olgazskiba/Java_blog/JavaBlogApplication.java
@@ -0,0 +1,13 @@
+package io.github.olgazskiba.Java_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/olgazskiba/Java_blog/JavaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.olgazskiba.Java_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
